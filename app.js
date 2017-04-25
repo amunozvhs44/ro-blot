8e5295f939a7e3bd64f2904097855e2e6afb6a1b
Ana Munoz <382689@cds.ci.la.ca.us>
2017-04-25 13:10:15 -0700

    commit to changes in chat bot info

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+     appId: 'c9714d91-62cc-408c-a3ce-76ea105d7347', 
+     appPassword: 'n80ZqSRhF0sj8VYiEePPm0d' 
+    }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
